6b9cdb5996a8a830142b3b2513f9243bd7119e3a
David Rodriguez Alonso <d.rodriguezalon@alumnos.urjc.es>
2019-04-18 18:31:45 +0200

    Añado icono en ActivityMain con fondo

--- Thermos/app/src/main/java/alonsod/mov/urjc/thermos/MainActivity.java
+++ Thermos/app/src/main/java/alonsod/mov/urjc/thermos/MainActivity.java
@@ -1,13 +1,36 @@
 package alonsod.mov.urjc.thermos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class ThermosUtils {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setImage();
+    }
+
+    private void setImage() {
+        String img_level = "ic_icon01";
+        int id = getResources().getIdentifier(img_level, "drawable", getPackageName());
+        ImageView imgv = findViewById(R.id.img_app);
+        imgv.setImageResource(id);
+    }
+
+    public void start() {
+
+    }
+
+    public void help() {
+        Intent help = new Intent(MainActivity.this, HelpActivity.class);
+        startActivity(help);
     }
 }

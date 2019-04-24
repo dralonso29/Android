4e2547329ce632cbcd5e05441be502a17d6e2ecb
David Rodriguez Alonso <d.rodriguezalon@alumnos.urjc.es>
2019-04-24 19:44:43 +0200

    Quito imports y clases que no uso

--- Thermos/app/src/main/java/alonsod/mov/urjc/thermos/MainActivity.java
+++ Thermos/app/src/main/java/alonsod/mov/urjc/thermos/MainActivity.java
@@ -7,22 +7,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ConnectException;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
-import static alonsod.mov.urjc.thermos.Messages.TypeMessage.REP_MACHINE_TEMP;
-import static alonsod.mov.urjc.thermos.Messages.getTypeMessage;
-import static alonsod.mov.urjc.thermos.Messages.readFrom;
-
 public class MainActivity extends AppCompatActivity {
 
-    public class ThermosUtils {
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

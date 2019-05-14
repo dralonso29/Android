034342c5930fd972313b2078671bbaddf86da48c
David Rodriguez Alonso <d.rodriguezalon@alumnos.urjc.es>
2019-05-14 21:34:56 +0200

    Cada X segundos se hace una peticion al sever y mira la temperatura

--- Thermos/app/src/main/java/alonsod/mov/urjc/thermos/ThermosService.java
+++ Thermos/app/src/main/java/alonsod/mov/urjc/thermos/ThermosService.java
@@ -7,10 +7,13 @@ import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -26,43 +29,52 @@ public class ThermosService extends IntentService {
             @Override
             public void run(){
                 try {
-                    long endTime = System.currentTimeMillis() + WAITING_TIME;
-                    Socket s = new Socket(ip, PORT);
-                    DataInputStream dis = new DataInputStream(s.getInputStream());
-                    DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-                    Log.d("ThermosService","Hay que segui con el servicio? --> "+checkAlarm(dis, dos));
-                    /*while(checkAlarm(dis, dos)) {
-                        wait(endTime - System.currentTimeMillis());
-                    }*/
+                    while(true) {
+                        long endTime = System.currentTimeMillis() + WAITING_TIME;
+                        Socket s = new Socket(ip, PORT);
+                        DataInputStream dis = new DataInputStream(s.getInputStream());
+                        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+                        //Log.d("ThermosService","Hay que segui con el servicio? --> "+checkAlarm(dis, dos, ip));
+                        if (!checkAlarm(dis, dos, ip)) {
+                            dis.close();
+                            dos.close();
+                            break;
+                        }
+                        dis.close();
+                        dos.close();
+                        sleep(endTime - System.currentTimeMillis());
+                        Log.d("ThermosService", "Esperando...");
+                    }
+
                 }catch (ConnectException e) {
                     System.out.println("Connection refused "+ e);
                 }catch (UnknownHostException e) {
                     System.out.println("Cannot connect to host "+ e);
                 }catch (IOException e) {
                     System.out.println("IOExcepton "+ e);
-                } /*catch (InterruptedException e) {
+                } catch (InterruptedException e) {
                     e.printStackTrace();
-                }*/
+                }
             }
 
-            private boolean checkAlarm(DataInputStream dis, DataOutputStream dos) {
+            private boolean checkAlarm(DataInputStream dis, DataOutputStream dos, String machine) {
                 Log.d("ThermosService", "Dentro de checkAlarm");
                 boolean runService = true;
                 Messages.RequestClient rc = new Messages.RequestClient();
                 rc.writeTo(dos);
                 Messages replyFromServer = Messages.readFrom(dis);
-                int alarm = 30;
-                String machine = "MaquinaPrueba";
+                int alarm = getAlarm();
+                //String machine = "MaquinaPrueba";
                 if (replyFromServer != null) {
                     String answer = null;
                     try {
                         answer = dis.readUTF();
                         Log.d("ThermosService", "Respuesta: " + answer);
-                        if (Integer.parseInt(answer) >= alarm) {
-                            showNotification(machine, alarm);
+                        int temp = Integer.parseInt(answer)/1000;
+                        if (temp >= alarm) {
+                            showNotification(machine, temp, alarm);
                             runService = false;
                         }
-
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -85,20 +97,24 @@ public class ThermosService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        String ip = "10.0.0.8";
-        int port  = 25029;
-        synchronized (this) {
-            try {
-                getTemp(ip, port);
-            } catch (Exception e) {
-                e.printStackTrace();
+        String ip = getMachine();
+        int port  = getPort();//25029;
+        Log.d("ThermosService", "Hacemos getMachine: "+getMachine());
+        Log.d("ThermosService", "Hacemos getPort: "+getPort());
+        if ((ip != null) && !ip.equals("null")) {
+            synchronized (this) {
+                try {
+                    getTemp(ip, port);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
-    private void showNotification(String machine, int alarm) {
-        String textTitle = "Atención que se prende la wea";
-        String textContent = "Maquina: "+machine+", con alarma: "+alarm;
+    private void showNotification(String machine, int temperatura, int alarm) {
+        String textTitle = "¡Atención, hay una máquina calentándose!";
+        String textContent = "Maquina: "+machine+" con temperatura de "+temperatura+" grados, ha superado la alarma de "+alarm+" grados";
 
         NotificationManager nmng =
                 (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
@@ -111,5 +127,51 @@ public class ThermosService extends IntentService {
                 .build();
 
         nmng.notify(NOTIFICATION_ID, notification);
+    }
+
+    private int getAlarm() {
+        String FILENAME = "save_Alarm";
+        int alarm = 0;
+        try {
+            FileInputStream in = openFileInput(FILENAME);
+            alarm = in.read();
+            Log.d("ThermosService", "getAlarm: alarma = "+alarm);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return alarm;
+    }
+
+    private int getPort() {
+        String FILENAME = "save_Port";
+        int port = 0;
+        String sPort = null;
+        try {
+            FileInputStream in = openFileInput(FILENAME);
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            sPort = bufferedReader.readLine();
+            port = Integer.parseInt(sPort);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return port;
+    }
+
+    private String getMachine() {
+        String FILENAME = "save_Machine";
+        String machine = null;
+        try {
+            FileInputStream in = openFileInput(FILENAME);
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            machine = bufferedReader.readLine();
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return machine;
     }
 }
